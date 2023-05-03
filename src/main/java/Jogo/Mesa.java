59164608fbea5e6bf8238f774fc5c68ddd3cbe41
Gabriel B. Lisboa <gabriel.lisboa11@gmail.com>
2023-05-03 11:54:02 -0300

    atualizei2

--- src/main/java/Jogo/Mesa.java
+++ src/main/java/Jogo/Mesa.java
@@ -12,7 +12,7 @@ import static Jogo.Jogadas.*;
 
 public class Mesa {
 
-// aqui é onde começa o jogo
+// aqui é onde começa o jogo só que não
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String querNovaCarta;
